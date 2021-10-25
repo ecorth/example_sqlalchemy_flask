6aada3fbf1a818a1cc1db6e6d3ba6a491bae93da
Héctor Luna Arriaga <hluna@cxc.com.mx>
2021-10-25 01:06:57 -0500

    Se hace refactor de app.py

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
+from flask_jwt import JWT
 from flask import Flask
+from flask_restful import Api
+from resources.item import Item, ItemList
+from resources.user import UserRegister
+from security import authenticate, identity
+from db import db
 
 app = Flask(__name__)
+db.init_app(app)
+# no change structure of database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'super-secret'
+app.debug = True
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     app.run()
